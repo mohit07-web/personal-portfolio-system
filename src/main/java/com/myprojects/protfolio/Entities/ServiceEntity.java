925daf92b9158e28028cb4ca61dc200e12d2ae09
mohit07-web <jrrmohit2052@gmail.com>
2025-09-13 13:38:57 +0530

    update

--- src/main/java/com/myprojects/protfolio/Entities/ServiceEntity.java
+++ src/main/java/com/myprojects/protfolio/Entities/ServiceEntity.java
@@ -25,7 +25,7 @@ public class ServiceEntity {
 	@Column(length = 30)
 	private String title;
 	
-	@Column(length = 80)
+	@Column(length = 100)
 	private String description;
 
 	@Column(length = 300)
